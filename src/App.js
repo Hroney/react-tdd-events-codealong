aa52d2289ecf8c5554c4df722378735a29948f79
Hayden Roney <roneyhayden@gmail.com>
2024-01-13 09:14:26 -0600

    lab done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React, { useState } from "react";
+
 function App() {
+  const [pizzaToppings, setPizzaToppings] = useState(["cheese"]);
+  const [pepperoniChecked, setPepperoniChecked] = useState(pizzaToppings.includes("pepperoni"));
+
+  function handleCheckBox(event) {
+    const value = event.target.name;
+    const isChecked = event.target.checked;
+
+    setPepperoniChecked(isChecked);
+
+    if (pizzaToppings.includes(value)) {
+      setPizzaToppings(pizzaToppings.filter((topping) => topping !== value));
+    } else {
+      setPizzaToppings([...pizzaToppings, value]);
+    }
+  }
+
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Select Pizza Topping</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        name="pepperoni"
+        checked={pepperoniChecked}
+        onChange={handleCheckBox}
+      />
+      <label htmlFor="pepperoni">{pepperoniChecked ? "Take off Pepperoni" : "Add Pepperoni"}</label>
+      <ul>
+        {pizzaToppings.map((topping) => {
+          return <li key={topping}>{topping}</li>
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,47 @@
-import { render, screen } from "@testing-library/react";
+import { logRoles, render, screen } from "@testing-library/react";
 import App from "../App";
 
 import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
 
 // Code tests here
+
+test("pizza checkbox is initially unchecked", () => {
+    render(<App />);
+
+    const addPepperoni = screen.getByRole("checkbox", {
+        value: /add pepperoni/i
+    })
+
+    expect(addPepperoni).not.toBeChecked();
+})
+test("toppings list initially contains only cheese", () => {
+    render(<App />);
+
+    expect(screen.getByText(/cheese/i)).toBeInTheDocument();
+})
+test("checkbox appears as checked when user clicks it", () => {
+    render(<App />);
+
+    const addPepperoni = screen.getByRole("checkbox", { name: /pepperoni/i })
+
+    userEvent.click(addPepperoni)
+    expect(addPepperoni).toBeChecked();
+})
+test("topping appears in toppings list when checked", () => {
+    render(<App />)
+
+    const addPepperoni = screen.getByRole("checkbox", { name: /pepperoni/i })
+
+    userEvent.click(addPepperoni)
+
+    expect(addPepperoni).toBeChecked();
+    expect(screen.getByText("cheese")).toBeInTheDocument();
+    expect(screen.getByText("pepperoni")).toBeInTheDocument();
+
+    userEvent.click(addPepperoni)
+
+    expect(addPepperoni).not.toBeChecked();
+    expect(screen.getByText("cheese")).toBeInTheDocument();
+    expect(screen.queryByText("pepperoni")).not.toBeInTheDocument();
+})
